3f3604a6cecd953d9e29cc30d903030816595ec3
SimonSchneZzer <simsch2002@gmail.com>
2025-06-19 14:35:12 +0200

    base code

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log("App is running");
+
+const chartDiv = document.getElementById("chart");
+const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg.setAttribute("width", "100%");
+svg.setAttribute("height", "300");
+svg.innerHTML = `<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">D3 Chart Here</text>`;
+chartDiv.appendChild(svg);
