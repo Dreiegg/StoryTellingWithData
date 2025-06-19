fe95016b13d3636037deb6fe34532230722c8b05
SimonSchneZzer <simsch2002@gmail.com>
2025-06-19 15:12:55 +0200

    darkmode and font

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@ function insertDummyChart(containerId) {
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("width", "100%");
   svg.setAttribute("height", "300");
-  svg.innerHTML = `<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">
-    D3 Chart Here
-  </text>`;
+  svg.innerHTML = `
+    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">
+      D3 Chart Here
+    </text>`;
   chartDiv.appendChild(svg);
 }
 
